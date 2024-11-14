74f1b934c4e80386372d0d32c7d9c450e392c0b4
Shlumpus <140124184+Shlumpus@users.noreply.github.com>
2024-11-14 01:38:26 +0000

    starting to create my big ol' movie list. Incorporated functions aswell as things to use from sqlite inside of those functions. Made a main function

--- app.py
+++ app.py
@@ -1,8 +1,21 @@
 import sqlite3
 
 conn = sqlite3.connect('movies.db')
-
 cursor = conn.cursor()
+
+# Select all movies
+
+cursor.execute('SELECT * FROM movies')
+all_movies = cursor.fetchall()
+print("All movies:")
+for movie in all_movies:
+    print(movie)
+
+cursor.execute('''
+UPDATE movies
+SET rating = 9.0
+WHERE title = 'Inception'
+''')
 
 # Create the movies table
 cursor.execute('''
@@ -13,8 +26,14 @@ CREATE TABLE IF NOT EXISTS movies (
     year INTEGER,
     rating FLOAT
 )
-
 ''')
+conn.commit()
 
+movie = ('', 'Francis Ford Coppola', 1972, 9.2)
+
+cursor.execute(f'''
+INSERT INTO movies (title, director, year, rating)
+VALUES ('{movie[0]}', '{movie[1]}', {movie[2]}, {movie[3]})
+''')
 conn.commit()
 conn.close()
